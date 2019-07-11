e8fda5cf1171180c37b09ce24041e0fc632ac621
yuqiang1 <yq18575674182@aliyun.com>
2019-07-11 23:14:48 +0800

    修改

--- blog/src/layouts/default.vue
+++ blog/src/layouts/default.vue
@@ -104,14 +104,15 @@
       </footer>
   </div>
 </template>
-<script>
-// import '~/assets/plugins/normalize-css/normalize.css'
-// import '~/assets/plugins/yui/cssgrids-min.css'
-// import '~/assets/plugins/sui/sui.min.css'
-// import '~/assets/plugins/sui/sui-append.min.css'
-// import '~/assets/plugins/font-awesome/css/font-awesome.min.css'
-// import '~/assets/css/widget-base.css'
-// import '~/assets/css/widget-head-foot.css'
+<script> 
+
+import'../assets/plugins/normalize-css/normalize.css'
+import '../assets/plugins/yui/cssgrids-min.css'
+import '../assets/plugins/sui/sui.min.css'
+import '../assets/plugins/sui/sui-append.min.css'
+import '../assets/plugins/font-awesome/css/font-awesome.min.css'
+import '../assets/css/widget-base.css'
+import '../assets/css/widget-head-foot.css'
 //import {getUser,removeUser} from '@/utils/auth'
 export default {
     data(){
